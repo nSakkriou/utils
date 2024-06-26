5aaf389ae02198d40fd859a287ac75dde5259881
Nathan SAKKRIOU <nathansakkriou@gmail.com>
2024-06-26 16:49:26 +0200

    add cors options

--- pkg/agent/types.go
+++ pkg/agent/types.go
@@ -4,10 +4,17 @@ type AgentFile struct {
 	Port              int          `json:"port"`
 	ScriptsFolderPath string       `json:"scripts_folder_path"`
 	EndCommands       []EndCommand `json:"endcommands"`
+
+	UseCors    bool       `json:"use_cors"`
+	CorsOption CorsOption `json:"cors_option"`
 }
 
 type EndCommand struct {
 	EndpointName      string   `json:"endpoint_name"`
 	Method            string   `json:"method"`
 	ScriptsFilesNames []string `json:"scripts_files_names"`
+}
+
+type CorsOption struct {
+	AllowedOrigins []string `json:"allowed_origins"`
 }
